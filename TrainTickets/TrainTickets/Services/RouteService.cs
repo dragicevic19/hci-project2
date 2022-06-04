78fa9e7f23d2b0a72fdee65a209aed517ef1c96e
Boki0 <bojan.vasic01@gmail.com>
2022-06-04 16:07:16 +0200

    metoda za pretragu rute sa vracenom cenom vremenom

--- TrainTickets/TrainTickets/Services/RouteService.cs
+++ TrainTickets/TrainTickets/Services/RouteService.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrainTickets.dto;
+using TrainTickets.model.station;
+using TrainTickets.Database;
 
 namespace TrainTickets.Services
 {
     internal class RouteService
     {
+        public List<RoutesForViewWithPriceDTO> routesWithPriceAndTime(Station start, Station end)//vraca sve polaske ovde moze da se izbaci iz dto-a  timeonly departure times pa samim tim i ceo ovaj for drugi
+        {
+            List<RoutesForViewWithPriceDTO> routes = new List<RoutesForViewWithPriceDTO>(); 
+            using (var db = new DatabaseContext())
+            {
+                foreach (var route in db.TrainRoutes)
+                {
+                    if(route.containsRoute(start, end))
+                    {
+                        foreach(var time in route.DepartureTimes)
+                        {
+                            routes.Add(new RoutesForViewWithPriceDTO(route, start, end, route.routePrice(start, end), time, route.routeTime(start, end)));
+                        }    
+
+                    }
+                     
+                }
+            }
+
+            return routes;
+
+        }
+
+
     }
 }
